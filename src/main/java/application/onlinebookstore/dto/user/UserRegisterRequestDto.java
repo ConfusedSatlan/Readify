51f288f7cd4624659ff82f4a870369a553a8993f
Mykyta Zuber <nikitazuber@gmail.com>
2023-09-13 19:07:02 +0300

    added FieldMatch

--- src/main/java/application/onlinebookstore/dto/user/UserRegisterRequestDto.java
+++ src/main/java/application/onlinebookstore/dto/user/UserRegisterRequestDto.java
@@ -1,9 +1,15 @@
 package application.onlinebookstore.dto.user;
 
+import application.onlinebookstore.validation.FieldMatch;
 import application.onlinebookstore.validation.Password;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
+@FieldMatch(
+        first = "password",
+        second = "repeatPassword",
+        message = "Passwords don't match"
+)
 public record UserRegisterRequestDto(@NotNull
                                      @Email
                                      String email,
