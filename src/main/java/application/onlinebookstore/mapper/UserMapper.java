07b860b0e7d67c05f512746698747e2e29f268f9
Mykyta Zuber <nikitazuber@gmail.com>
2023-09-17 00:13:17 +0300

    fixed code

--- src/main/java/application/onlinebookstore/mapper/UserMapper.java
+++ src/main/java/application/onlinebookstore/mapper/UserMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(config = MapperConfig.class)
 public interface UserMapper {
-    UserResponseDto toUserResponse(User user);
+    UserResponseDto toUserResponseDto(User user);
 
     User toModel(UserRegisterRequestDto requestDto);
 }
--- src/main/java/application/onlinebookstore/validation/impl/PasswordValidator.java
+++ src/main/java/application/onlinebookstore/validation/impl/PasswordValidator.java
@@ -6,12 +6,12 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.util.regex.Pattern;
 
 public class PasswordValidator implements ConstraintValidator<Password, String> {
-    private static final String PATTERN_OF_PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d).{6,}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d).{6,}$";
 
     @Override
     public boolean isValid(String password,
                            ConstraintValidatorContext constraintValidatorContext) {
-        return password != null && Pattern.compile(PATTERN_OF_PASSWORD)
+        return password != null && Pattern.compile(PASSWORD_PATTERN)
                 .matcher(password).matches();
     }
 }
