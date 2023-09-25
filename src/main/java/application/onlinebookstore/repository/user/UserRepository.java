60d6992304c3927ce6eefd2c53d118adc52ca2d1
Mykyta Zuber <nikitazuber@gmail.com>
2023-09-25 12:52:27 +0300

    fixed code

--- src/main/java/application/onlinebookstore/repository/user/UserRepository.java
+++ src/main/java/application/onlinebookstore/repository/user/UserRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserRepository extends JpaRepository<Users, Long>, JpaSpecificationExecutor<Users> {
+public interface UserRepository extends JpaRepository<Users, Long>,
+        JpaSpecificationExecutor<Users> {
     Optional<Users> findByEmail(String email);
 }
