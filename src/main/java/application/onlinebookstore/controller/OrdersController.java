d5d35f1033c40bbd10948af57f6916dd75225164
Mykyta Zuber <nikitazuber@gmail.com>
2023-09-26 16:01:33 +0300

    fixed code

--- src/main/java/application/onlinebookstore/controller/OrdersController.java
+++ src/main/java/application/onlinebookstore/controller/OrdersController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
